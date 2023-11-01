c2a455273720283a02b3a5e9a4ba9a6e507710c0
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-11-01 21:51:46 +0600

    first images size doubled

--- src/Components/ImageGallery.jsx
+++ src/Components/ImageGallery.jsx
@@ -69,7 +69,7 @@ const ImageGallery = () => {
             )}
             <div className="grid grid-cols-1 gap-8 md:grid-cols-5 checked:brightness-75">
                 {images.map((image, index) => (
-                    <div key={image.id} className="relative group hover:brightness-50">
+                    <div key={image.id} className="relative group image-container hover:brightness-50">
                         <img
                             src={URL.createObjectURL(image.file)}
                             alt={`Image ${index}`}
